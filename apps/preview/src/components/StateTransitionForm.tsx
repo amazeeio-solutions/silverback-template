9a25a603b16248369f45cde3bd281c756f292893
Vasile Chindris <vasile.chindris@amazee.com>
2025-01-30 16:40:25 +0200

    chore(SLB-448): first working POC, non styling, still work in progress

--- apps/preview/src/components/StateTransitionForm.tsx
+++ apps/preview/src/components/StateTransitionForm.tsx
@@ -1,3 +1,4 @@
+import { useIntl } from '@amazeelabs/react-intl';
 import {
   ModerateContentMutation,
   OperationVariables,
@@ -34,7 +35,7 @@ export default function StateTransitionForm() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const { data, trigger } = useMutation(ModerateContentMutation);
+  const { data, trigger, isMutating } = useMutation(ModerateContentMutation);
   const { ...previewParams } = usePreviewParameters();
 
   const {
@@ -42,9 +43,42 @@ export default function StateTransitionForm() {
     isLoading: previewIsLoading,
     error: previewError,
   } = useOperation(PreviewDrupalPageQuery, previewParams);
-  console.log('Preview data: ', previewData);
-  //const transitions
 
+  const intl = useIntl();
+
+  const errorMessages =
+    !isMutating &&
+    data &&
+    data.moderateContent?.errors &&
+    data.moderateContent.errors.length > 0
+      ? data.moderateContent.errors.map((error) => {
+          return error?.message || '';
+        })
+      : null;
+  const successMessage =
+    !isMutating && data && data.moderateContent?.result === 'success'
+      ? intl.formatMessage({
+          defaultMessage: 'The moderation state has been updated.',
+          id: 'hZBzvI',
+        })
+      : null;
+
+  if (previewIsLoading) {
+    return null;
+  }
+  if (previewError) {
+    return (
+      <div>
+        <p>
+          {intl.formatMessage({
+            defaultMessage:
+              'The moderation state transition form cannot be displayed because the content failed to load.',
+            id: 'mFbroE',
+          })}
+        </p>
+      </div>
+    );
+  }
   return (
     <div>
       <form
@@ -53,24 +87,95 @@ export default function StateTransitionForm() {
             contentId: previewParams.id,
             // We only support the node entity type for now.
             entityType: 'node',
+            revisionId: previewParams.rid || '',
             submittedData: JSON.stringify(values),
+            accessCredentials: {
+              previewUserId: previewParams.preview_user_id || '',
+              previewAccessToken: previewParams.preview_access_token || '',
+            },
           });
         })}
       >
-        {errors.name && <p>Name is required.</p>}
-        {errors.comment && <p>Comment is required.</p>}
+        {/* Error / success messages after the form has been submittd. */}
+        {successMessage ? (
+          <ul>
+            <li>{successMessage}</li>
+          </ul>
+        ) : null}
+        {errorMessages ? (
+          <ul>
+            {errorMessages.map((message, index) => (
+              <li key={index}>{message}</li>
+            ))}
+          </ul>
+        ) : null}
+        {/* Errors from the form validation on client side. */}
+        {errors && (
+          <ul>
+            {errors.name && (
+              <li>
+                {intl.formatMessage(
+                  {
+                    defaultMessage: '{field} is required.',
+                    id: 'pc9YT+',
+                  },
+                  {
+                    field: intl.formatMessage({
+                      defaultMessage: 'Name',
+                      id: 'HAlOn1',
+                    }),
+                  },
+                )}
+              </li>
+            )}
+            {errors.comment && (
+              <li>
+                {intl.formatMessage(
+                  {
+                    defaultMessage: '{field} is required.',
+                    id: 'pc9YT+',
+                  },
+                  {
+                    field: intl.formatMessage({
+                      defaultMessage: 'Comment',
+                      id: 'LgbKvU',
+                    }),
+                  },
+                )}
+              </li>
+            )}
+          </ul>
+        )}
         <div>
-          <label>Current state: </label>
+          <label>
+            {intl.formatMessage({
+              defaultMessage: 'Current state:',
+              id: 'UiUHpE',
+            })}{' '}
+          </label>
           <span>
             {previewData?.preview?.moderationInfo?.currentState?.label}
           </span>
         </div>
         <div>
-          <label>Change to: </label>
+          <label>
+            {intl.formatMessage({
+              defaultMessage: 'Change to:',
+              id: 'vcq+xk',
+            })}{' '}
+          </label>
           <select {...register('new_moderation_state')}>
             {previewData?.preview?.moderationInfo?.availableStates?.map(
               (availableState) => (
-                <option key={availableState?.id} value={availableState?.id}>
+                <option
+                  key={availableState?.id}
+                  value={availableState?.id}
+                  selected={
+                    availableState?.id ===
+                      previewData?.preview?.moderationInfo?.currentState?.id ||
+                    undefined
+                  }
+                >
                   {availableState?.label}
                 </option>
               ),
@@ -78,15 +183,35 @@ export default function StateTransitionForm() {
           </select>
         </div>
         <div>
-          <label>Name: </label>
+          <label>
+            {intl.formatMessage({
+              defaultMessage: 'Name:',
+              id: 'WF8jKB',
+            })}{' '}
+          </label>
           <input {...register('name', { required: true })} />
         </div>
         <div>
-          <label>Comment: </label>
+          <label>
+            {intl.formatMessage({
+              defaultMessage: 'Comment:',
+              id: '4FQrdH',
+            })}{' '}
+          </label>
           <textarea {...register('comment', { required: true })} />
         </div>
         <div>
-          <input type="submit" />
+          <button type="submit" disabled={isMutating}>
+            {isMutating
+              ? intl.formatMessage({
+                  defaultMessage: 'Sending...',
+                  id: '82Y7Sa',
+                })
+              : intl.formatMessage({
+                  defaultMessage: 'Submit',
+                  id: 'wSZR47',
+                })}
+          </button>
         </div>
       </form>
     </div>
--- packages/drupal/custom/src/ContentModeration.php
+++ packages/drupal/custom/src/ContentModeration.php
@@ -2,12 +2,17 @@
 
 namespace Drupal\custom;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\content_moderation\ModerationInformationInterface;
 use Drupal\content_moderation\StateTransitionValidationInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\RevisionableInterface;
+use Drupal\Core\Entity\RevisionableStorageInterface;
+use Drupal\Core\Entity\RevisionLogInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Session\AccountSwitcherInterface;
 use Drupal\Core\Session\UserSession;
+use Drupal\Core\Utility\Error;
 use Drupal\graphql_directives\DirectiveArguments;
 use Drupal\silverback_preview_link\Access\PreviewLinkAccessCheck;
 
@@ -41,12 +46,21 @@ class ContentModeration {
    */
   protected AccountSwitcherInterface $accountSwitcher;
 
+
+  /**
+   * The datetime service.
+   */
+  protected TimeInterface $time;
+
   /**
    * Constructs a ContentModeration object.
    *
    * @param \Drupal\content_moderation\ModerationInformationInterface $moderation_information
    * @param \Drupal\content_moderation\StateTransitionValidationInterface $state_validator
    * @param \Drupal\Core\Session\AccountInterface $current_user
+   * @param \Drupal\silverback_preview_link\Access\PreviewLinkAccessCheck $preview_link_access_check
+   * @param \Drupal\Core\Session\AccountSwitcherInterface $account_switcher
+   * @param \Drupal\Component\Datetime\TimeInterface $time
    */
   public function __construct(
     ModerationInformationInterface $moderation_information,
@@ -54,20 +68,81 @@ class ContentModeration {
     AccountInterface $current_user,
     PreviewLinkAccessCheck $preview_link_access_check,
     AccountSwitcherInterface $account_switcher,
+    TimeInterface $time
   ) {
     $this->moderationInformation = $moderation_information;
     $this->stateValidator = $state_validator;
     $this->currentUser = $current_user;
     $this->previewLinkAccessCheck = $preview_link_access_check;
     $this->accountSwitcher = $account_switcher;
+    $this->time = $time;
   }
 
   /**
    * Directive to moderate content.
    */
   public function moderateContent(DirectiveArguments $args) : array {
-    \Drupal::logger('debug')->info('Content moderated!: ' . $args->args['contentId'] . ' (' . $args->args['contentType'] . ')');
-    return [];
+    $userSessionChanged = FALSE;
+    try {
+      // For the moderateContent action, the access credentials should be inside
+      // the directive arguments.
+      // @todo: this might need a bigger refactoring, so that the request gets
+      // authenticated based on the preview_user_id and preview_access_token
+      // globally, so we do not need to perform the authorisation in this
+      // method.
+      $previewUserId = $args->args['accessCredentials']['previewUserId'];
+      $previewAccessToken = $args->args['accessCredentials']['previewAccessToken'];
+      /* @var \Drupal\Core\Entity\RevisionableStorageInterface $storage */
+      $storage = \Drupal::entityTypeManager()
+        ->getStorage($args->args['entityType']);
+      if (!$storage instanceof RevisionableStorageInterface) {
+        throw new \Exception("The " . $args->args['entityType'] . ' entity storage is not revisionable.');
+      }
+      $content = $storage->loadRevision($args->args['revisionId']);
+      if (!empty($previewAccessToken) && !empty($previewUserId) && $this->currentUser->id() != $previewUserId) {
+        $accessResult = $this->previewLinkAccessCheck->access($content, $previewAccessToken);
+        // If the access token is valid, then we should switch to preview user
+        // as well.
+        if ($accessResult->isAllowed()) {
+          $this->accountSwitcher->switchTo(new UserSession(['uid' => $previewUserId]));
+          $userSessionChanged = TRUE;
+        }
+      }
+      $submittedData = json_decode($args->args['submittedData'], true);
+      // Check again if the transition is allowed.
+      $workflow = $this->moderationInformation->getWorkflowForEntity($content);
+      $currentState = $this->moderationInformation->getOriginalState($content);
+      $newState = $workflow->getTypePlugin()->getState($submittedData['new_moderation_state']);
+      if (!$this->stateValidator->isTransitionValid($workflow, $currentState, $newState, $this->currentUser, $content)) {
+        throw new \Exception("The moderation state could not be changed. Transition from " .$currentState->id() . ' to  ' . $newState->id() . ' is not allowed (' . $args->args['entityType'] . '; ' . $args->args['contentId'] . ') for user id: ' .$this->currentUser->id());
+      }
+
+      // If we get here, then we are allowed to change the moderation state, so
+      // we try to do it.
+      $this->changeModerationStateForContent($content, $submittedData['new_moderation_state'], $submittedData['comment'], $submittedData['name']);
+      return [
+        'result' => 'success',
+        'errors' => NULL,
+      ];
+    }
+    catch (\Exception $e) {
+      Error::logException(\Drupal::logger('error'), $e);
+      return [
+        'result' => 'error',
+        'errors' => [
+          [
+            'message' => 'The moderation state could not be changed. Check the application logs for more details.',
+            'key' => 'invalid_input'
+          ]
+        ],
+      ];
+    }
+    finally {
+      // Make sure we switch back to the original session.
+      if ($userSessionChanged) {
+        $this->accountSwitcher->switchBack();
+      }
+    }
   }
 
   /**
@@ -167,5 +242,31 @@ class ContentModeration {
       'preview_access_token' => $previewAccessToken,
       'preview_user_id' => $previewUserId,
     ];
+  }
+
+  /**
+   * Helper method to change the moderation state of a cnotent.
+   *
+   * @param \Drupal\Core\Entity\RevisionableInterface $content
+   * @param string $new_state
+   * @param string $comment
+   * @param string $author_name
+   *
+   * @return void
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function changeModerationStateForContent(RevisionableInterface $content, string $new_state, string $comment, string $author_name) {
+    /* @var \Drupal\Core\Entity\RevisionableStorageInterface $storage */
+    $storage = \Drupal::entityTypeManager()->getStorage($content->getEntityTypeId());
+    $content = $storage->createRevision($content, $content->isDefaultRevision());
+    $content->set('moderation_state', $new_state);
+    if ($content instanceof RevisionLogInterface) {
+      $content->setRevisionCreationTime($this->time->getRequestTime());
+      $content->setRevisionLogMessage($comment . ' (' . $author_name . ')');
+      $content->setRevisionUserId($this->currentUser->id());
+    }
+    $content->save();
   }
 }
