a6813cfbabfd0d87276659ad73ca15dd1f6244e8
Philipp Melab <philipp.melab@gmail.com>
2025-04-03 09:50:42 +0200

    refactor(cms): use turbo cache for drupal installs

--- apps/cms/prep-database.sh
+++ apps/cms/prep-database.sh
@@ -9,7 +9,11 @@ fi
 
 set -e
 
-pnpm drupal-install
+if ! test -f web/sites/default/files/.sqlite; then
+  pnpm drupal-install
+else
+  pnpm drush deploy -y
+fi
 
 # In any case, re-import translation string.
 pnpm import-translations
