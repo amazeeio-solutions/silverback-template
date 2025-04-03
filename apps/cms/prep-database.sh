f31444efa6fee35e9e7ed6eebdcb8c81c9d5eee7
Philipp Melab <philipp.melab@gmail.com>
2025-04-03 16:24:45 +0200

    ci: make translation import a dedicated task

--- apps/cms/prep-database.sh
+++ apps/cms/prep-database.sh
@@ -1,20 +1,6 @@
-if [ ! -z $LAGOON ]; then
-  # Do not touch database on Lagoon
-  exit 0
-fi
-
-if [ ! -z $SKIP_DRUPAL_INSTALL ]; then
-  exit 0
-fi
-
 set -e
-
 if ! test -f web/sites/default/files/.sqlite; then
   pnpm drupal-install
 else
   pnpm drush deploy -y
 fi
-
-# In any case, re-import translation string.
-pnpm import-translations
-pnpm drush cr
