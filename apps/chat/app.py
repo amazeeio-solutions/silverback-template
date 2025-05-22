c5a445911cafbbbc85b120f0e506b3b89927138b
Philipp Melab <philipp.melab@gmail.com>
2025-05-22 11:18:50 +0200

    refactor: switch to langchain

--- apps/chat/app.py
+++ apps/chat/app.py
@@ -1,46 +1,58 @@
-from typing import List
-from chainlit.mcp import McpConnection
-import json
-from openai import AsyncOpenAI
+from langchain.memory.chat_memory import BaseChatMemory
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema import StrOutputParser
+from langchain.chains.conversation.memory import ConversationBufferMemory
 import os
-from mcp import ClientSession
-import pprint
+from langchain.schema.runnable import Runnable, RunnablePassthrough
+from langchain.schema.runnable.config import RunnableConfig
+from typing import cast
 
 import chainlit as cl
-from openai.types.chat import ChatCompletionMessageParam
-
-print(os.environ.get("AMAZEEAI_API_KEY"))
-
-client = AsyncOpenAI(
-    api_key=os.environ.get("AMAZEEAI_API_KEY"),
-    # TODO: read this from drupal config?
-    base_url="https://llm.de103.amazee.ai",
-)
-
-cl.instrument_openai()
-pp = pprint.PrettyPrinter(indent=2).pprint
+from pydantic import SecretStr
 
 
 @cl.on_chat_start
 async def start_chat():
-    initialPrompt: ChatCompletionMessageParam = {
-        "role": "system",
-        "content": "You are a helpful chatbot.",
-    }
-    cl.user_session.set("history", [initialPrompt])
+    model = ChatOpenAI(
+        streaming=True,
+        api_key=cast(SecretStr, os.environ.get("AMAZEEAI_API_KEY")),
+        model="claude-3-5-sonnet",
+        base_url="https://llm.de103.amazee.ai",
+    )
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful chatbot.",
+            ),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{question}"),
+        ]
+    )
+    history = ConversationBufferMemory(return_messages=True)
+    runnable = (
+        RunnablePassthrough.assign(
+            history=lambda _: history.load_memory_variables({})["history"]
+        )
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+    cl.user_session.set("history", history)
+    cl.user_session.set("runnable", runnable)
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    history: List[ChatCompletionMessageParam] = cl.user_session.get("history") or []
-    history.append({"content": message.content, "role": "user"})
+    runnable = cast(Runnable, cl.user_session.get("runnable"))
+    history = cast(BaseChatMemory, cl.user_session.get("history"))
     msg = cl.Message(content="")
-    stream = await client.chat.completions.create(
-        messages=history,
-        model="claude-3-5-sonnet",
-        stream=True,
-    )
-    async for part in stream:
-        await msg.stream_token(part.choices[0].delta.content or "")
-    history.append({"role": "assistant", "content": msg.content})
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    history.save_context({"input": message.content}, {"output": msg.content})
     await msg.send()
