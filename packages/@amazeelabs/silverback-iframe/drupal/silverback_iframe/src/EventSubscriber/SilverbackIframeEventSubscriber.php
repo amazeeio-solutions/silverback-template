c1e79edb184e9908bc31b99698469d97ca1b8cf1
Eli Stone <2546188+elistone@users.noreply.github.com>
2025-05-29 09:46:37 +0100

    chore(slb-537): disable cache tests to see if they are required

--- packages/@amazeelabs/silverback-iframe/drupal/silverback_iframe/src/EventSubscriber/SilverbackIframeEventSubscriber.php
+++ packages/@amazeelabs/silverback-iframe/drupal/silverback_iframe/src/EventSubscriber/SilverbackIframeEventSubscriber.php
@@ -25,19 +25,19 @@ class SilverbackIframeEventSubscriber implements EventSubscriberInterface {
           $response->headers->set('Vary', implode(', ', $varyHeaders));
         }
 
-        // TEST OPTION 1: Force no caching for these requests
-        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
-        $response->headers->set('Pragma', 'no-cache');
-        $response->headers->set('Expires', '0');
-
-        // TEST OPTION 2: Add a random cache buster (alternative approach)
-        if (method_exists($response, 'getCacheableMetadata')) {
-          $randomValue = mt_rand(1, 1000000);
-          $response->getCacheableMetadata()->addCacheContexts(['url.query_args:ref', 'random:' . $randomValue]);
-
-          // Add debug header to verify the random value is changing
-          $response->headers->set('X-Cache-Buster', $randomValue);
-        }
+//        // TEST OPTION 1: Force no caching for these requests
+//        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
+//        $response->headers->set('Pragma', 'no-cache');
+//        $response->headers->set('Expires', '0');
+//
+//        // TEST OPTION 2: Add a random cache buster (alternative approach)
+//        if (method_exists($response, 'getCacheableMetadata')) {
+//          $randomValue = mt_rand(1, 1000000);
+//          $response->getCacheableMetadata()->addCacheContexts(['url.query_args:ref', 'random:' . $randomValue]);
+//
+//          // Add debug header to verify the random value is changing
+//          $response->headers->set('X-Cache-Buster', $randomValue);
+//        }
 
         // For debugging
         $response->headers->set('X-Ref-Source', $refValue);
