943e10357500ab7db488e2789a503e905dc03e0a
Dimitris Spachos <dimitris.spachos@amazeelabs.com>
2025-06-11 12:02:38 +0300

    feat(slb-531): add direct reference test, clean up SearchApiIndexUpdateTest and remove unused code

--- packages/@amazeelabs/silverback-search/drupal/silverback_search/tests/src/Kernel/SearchApiIndexUpdateTest.php
+++ packages/@amazeelabs/silverback-search/drupal/silverback_search/tests/src/Kernel/SearchApiIndexUpdateTest.php
@@ -4,24 +4,19 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\silverback_test\Kernel\Datasource;
 
-use Drupal\Core\DependencyInjection\ContainerBuilder;
-use Drupal\Core\Language\LanguageInterface;
 use Drupal\Tests\TestFileCreationTrait;
 use Drupal\Tests\media\Kernel\MediaKernelTestBase;
 use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
 use Drupal\Tests\node\Traits\NodeCreationTrait;
 use Drupal\Tests\search_api\Kernel\PostRequestIndexingTrait;
-use Drupal\Tests\search_api\Kernel\TestLogger;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\file\Entity\File;
 use Drupal\media\Entity\Media;
-use Drupal\media\Entity\MediaType;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
 use Drupal\search_api\Entity\Index;
 use Drupal\search_api\Entity\Server;
-use Drupal\search_api\Utility\TrackingHelper;
 use Drupal\search_api\Utility\Utility;
 use Drupal\silverback_gutenberg\BlockSerializer;
 
@@ -44,7 +39,6 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
     'node',
     'search_api',
     'search_api_test',
-    'search_api_test_example_content_references',
     'media',
     'path_alias',
     'filter',
@@ -83,12 +77,7 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
 
     $this->installEntitySchema('node');
     $this->installEntitySchema('search_api_task');
-    $this->installConfig([
-      'search_api',
-      'search_api_test_example_content_references',
-    ]);
-
-    $this->installConfig(['filter']);
+    $this->installConfig(['search_api', 'filter']);
 
     // Do not use a batch for tracking the initial items after creating an
     // index when running the tests via the GUI. Otherwise, it seems Drupal's
@@ -103,11 +92,6 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
     ])->save();
 
     $this->createMediaType('image', ['id' => 'image']);
-    $this->container->get('content_translation.manager')->setEnabled(
-      'media',
-      'image',
-      TRUE
-    );
 
     $pageType = NodeType::create(
       [
@@ -128,6 +112,20 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
       'entity_type' => 'node',
       'bundle' => 'page',
       'label' => 'Body',
+    ])->save();
+
+    // Direct entity reference field
+    FieldStorageConfig::create([
+      'field_name' => 'field_media',
+      'entity_type' => 'node',
+      'type' => 'entity_reference',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_media',
+      'entity_type' => 'node',
+      'bundle' => 'page',
+      'label' => 'Media reference direct',
     ])->save();
 
     $this->index = Index::create([
@@ -161,20 +159,7 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public function register(ContainerBuilder $container): void {
-    parent::register($container);
-
-    // Set a logger that will throw exceptions when warnings/errors are logged.
-    $logger = new TestLogger('');
-    $container->set('logger.factory', $logger);
-    $container->set('logger.channel.search_api', $logger);
-    $container->set('logger.channel.search_api_db', $logger);
-  }
-
-  /**
-   * Tests correct tracking of changes in referenced entities.
+   * Tests correct tracking of changes in referenced entities inside gutenberg blocks.
    *
    */
   public function testReferencedEntityChangedGutenbergBlock() {
@@ -227,13 +212,80 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
 
     $node = Node::create([
       'type' => 'page',
-      'title' => 'Editor test',
+      'title' => 'Media Gutenberg reference item test',
       'body' => $html,
     ]);
     $node->save();
 
     $this->index->indexItems();
     $tracker = $this->index->getTrackerInstance();
+
+    $this->assertEquals($tracker->getIndexedItemsCount(), 2);
+
+    $this->assertEquals(
+      [],
+      $tracker->getRemainingItems(),
+      '⚠️ Initial index matching error (1)',
+    );
+
+    // Updating the media will also trigger the re-index of the node.
+    $media->set('field_media_image', ['target_id' => $file2->id()]);
+    $media->save();
+
+    $expected[] = Utility::createCombinedId('entity:media', $media->id() . ':en');
+    $expected[] = Utility::createCombinedId('entity:node', $node->id() . ':en');
+
+    $this->assertEquals(
+      $expected,
+      $tracker->getRemainingItems(),
+      '⚠️ After media update index matching error (2)',
+    );
+
+    // Make sure that no unknown items were queued for post-request indexing.
+    $this->triggerPostRequestIndexing();
+  }
+
+  /**
+   * Tests correct tracking of changes in referenced entities outside gutenberg blocks.
+   *
+   */
+  public function testReferencedEntityChangedDirect() {
+
+    $file1 = File::create([
+      'uri' => $this->getTestFiles('image')[0]->uri,
+    ]);
+    $file1->save();
+
+    $file2 = File::create([
+      'uri' => $this->getTestFiles('image')[1]->uri,
+    ]);
+    $file2->save();
+
+    $media = Media::create([
+      'bundle' => 'image',
+      'name' => 'Screaming hairy armadillo',
+      'field_media_image' => [
+        [
+          'target_id' => $file1->id(),
+          'alt' => 'Screaming hairy armadillo',
+          'title' => 'Screaming hairy armadillo',
+        ],
+      ],
+    ]);
+    $media->save();
+
+    $node = Node::create([
+      'type' => 'page',
+      'title' => 'Media reference item test',
+      'body' => 'Body value',
+      'field_media' => ['target_id' => $media->id()],
+    ]);
+    $node->save();
+
+    $this->index->indexItems();
+    $tracker = $this->index->getTrackerInstance();
+
+    $this->assertEquals($tracker->getIndexedItemsCount(), 2);
 
     $this->assertEquals(
       [],
@@ -244,19 +296,13 @@ class SearchApiIndexUpdateTest extends MediaKernelTestBase {
     $media->set('field_media_image', ['target_id' => $file2->id()]);
     $media->save();
 
-    // $this->index->indexItems();
-    // $tracker->trackAllItemsUpdated(['1']);
-
     $expected[] = Utility::createCombinedId('entity:media', $media->id() . ':en');
     $expected[] = Utility::createCombinedId('entity:node', $node->id() . ':en');
 
     $this->assertEquals(
       $expected,
       $tracker->getRemainingItems(),
-      '⚠️ Media update index matching error (2)',
+      '⚠️ After media update index matching error (2)',
     );
-
-    // Make sure that no unknown items were queued for post-request indexing.
-    $this->triggerPostRequestIndexing();
   }
 }
