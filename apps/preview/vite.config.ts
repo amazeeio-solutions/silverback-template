9fabc86a354082f1d3ff0dd61c98126bc76b441e
Philipp Melab <philipp.melab@gmail.com>
2025-07-17 14:33:31 +0200

    feat(preview): enable access to UI package static assets

--- apps/preview/vite.config.ts
+++ apps/preview/vite.config.ts
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: resolve(__dirname, 'node_modules/@custom/ui/static/public'),
 });
