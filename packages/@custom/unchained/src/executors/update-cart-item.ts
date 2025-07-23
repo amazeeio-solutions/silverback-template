6c9c518714157bf7bd49ea5179f194cdd68e9967
Philipp Melab <philipp.melab@gmail.com>
2025-07-23 16:45:12 +0200

    WIP

--- packages/@custom/unchained/src/executors/update-cart-item.ts
+++ packages/@custom/unchained/src/executors/update-cart-item.ts
@@ -17,7 +17,7 @@ export function createUpdateCartItemExecutor(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const result = await client.request(UpdateCartItemMutation, vars as any);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return { data: result as any, error: null };
+    return result as any;
   };
 }
 
